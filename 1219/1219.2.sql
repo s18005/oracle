7ec23c9cee30d3fd01980d18fd0e5cd764fe9ed4
ryuma <s18005@std.it-college.ac.jp>
2018-12-20 05:14:54 +0000

    oracle kadai

---
+++ 1219/1219.2.sql
@@ -0,0 +1,7 @@
+select last_name, job_id, salary, commission_pct
+
+from employees
+
+where commission_pct is not null
+
+order by salary desc;
---
+++ 1219/1219.3.sql
@@ -0,0 +1,4 @@
+select last_name, nvl2(commission_pct, to_char(salary * commission_pct), 'No Commission') COMM
+
+from employees;
+
