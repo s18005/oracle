d7b0ff4ae74fb50e1d70a524bc67adb9f1d3821a
ryuma <s18005@std.it-college.ac.jp>
2019-02-06 06:32:37 +0000

    oracle kadai

---
+++ 0129/0129.1.sql
@@ -0,0 +1,8 @@
+create table dept (
+
+id number(7)
+     
+constraint test_dept_id_pk primary key,
+    
+name varchar2(25)
+);
---
+++ 0129/0129.2.sql
@@ -0,0 +1,5 @@
+insert into dept (id, name)
+
+select department_id, department_name
+
+from departments;
