1c38ac570d29b232e1dfbf6a2952b880106f284c
ryuma <s18005@std.it-college.ac.jp>
2019-02-06 05:04:09 +0000

    oracle kadai

---
+++ 0122/0122.3.sql
@@ -0,0 +1,13 @@
+select country_id, country_name
+
+from countries
+
+minus
+
+select country_id, country_name
+
+from countries join locations
+
+using (country_id) join departments
+
+using (location_id);
